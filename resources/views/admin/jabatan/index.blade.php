a0298ca9240c5cfc8dde39a3b9ec688b7b588d1f
ThoriqW <thoriqwajedi88@gmail.com>
2026-08-06 21:44:42 +0800

    fix lebar tabel unor, jabatan, pegawai

--- resources/views/admin/jabatan/index.blade.php
+++ resources/views/admin/jabatan/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="py-6">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between mb-6">
             <div>
                 <h1 class="text-2xl font-semibold text-gray-900">Daftar Jabatan</h1>
